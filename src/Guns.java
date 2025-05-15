e930a6f469fe115cbc1c310213b952d5b77e79d5
Carlos Andres Castrillon Oviedo <carlos.castrillonov@campusucc.edu.co>
2025-05-15 16:16:55 -0500

    class guns constructor

--- src/Guns.java
+++ src/Guns.java
@@ -2,6 +2,13 @@ public class Guns {
     private String name;
     private String type;
 
-
+    // constructor
+    public Guns (String name, String type){
+        this.name = name;
+        this.type = type;
     }
+    /**
+     * falta crear los metodos...
+     */
+
 }
