0285762fd66e4408fd6858cddc1e1e4bc68dfd5e
Carlos Andres Castrillon Oviedo <carlos.castrillonov@campusucc.edu.co>
2025-05-18 16:56:10 -0500

    implementacion de los metodos getter

--- src/Weapon.java
+++ src/Weapon.java
@@ -1,11 +1,32 @@
 public class Weapon {
     private String name;
     private String type;
+    private int damageMax = 40;
+    private int damageMin = 20;
+
 
     // constructor
     public Weapon(String name, String type){
         this.name = name;
         this.type = type;
+    }
+
+    // metodos getter
+
+    public String getName(){
+        return name;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public int getDamageMax(){
+        return damageMax;
+    }
+
+    public int getDamageMin(){
+        return damageMin;
     }
 
     // metodo
