1d76e605e4f5ccf2262853d470553d0734985797
Carlos Andres Castrillon Oviedo <carlos.castrillonov@campusucc.edu.co>
2025-05-18 17:43:35 -0500

    constructor

--- src/FightingGame.java
+++ src/FightingGame.java
@@ -2,5 +2,9 @@ public class FightingGame {
     private Character user1;
     private Character user2;
 
-
+    // constructor
+    public FightingGame(Character user1, Character user2){
+        this.user1 = user1;
+        this.user2 = user2;
+    }
 }
