775f247f773191c495579ce0227957df86ce1fcb
Carlos Andres Castrillon Oviedo <carlos.castrillonov@campusucc.edu.co>
2025-05-20 17:14:51 -0500

    se sobre escribe el metodo generateDamage agregando un condicional para bonificar o restar daño segun la subclase de Character que la usa

---
+++ src/Baculo.java
@@ -0,0 +1,20 @@
+public class Baculo extends Weapon{
+    public Baculo(String name, String type){
+        super("Mil almas del caos","Infernal");
+    }
+
+    /**
+     * estoy sobre escribe
+     *
+     */
+    @Override
+    public int generateDamage(Character user){
+        int baseDamage = super.generateDamage(); // asigno el valor base de la clase padre
+        if (user instanceof Mago){
+            return baseDamage + 5;
+        }else{
+            return baseDamage - 3;
+        }
+        return baseDamage;
+    }
+}
