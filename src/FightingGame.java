a97e31e50e9201770598ac8482c76bd97810571d
Carlos Andres Castrillon Oviedo <carlos.castrillonov@campusucc.edu.co>
2025-05-20 15:15:42 -0500

    se implementa el metodo startFight

--- src/FightingGame.java
+++ src/FightingGame.java
@@ -18,9 +18,35 @@ public class FightingGame {
         }
 
     }
+    // metodo startFight
+
+    public void startFight(){
+        System.out.println("Inicia la lucha entre " + "\n" +
+                            user1.getName() + "y " + user2.getName() );
+        int ronda = 1;
+
+        while(user1.isAlive() && user2.isAlive()){ // el loop funciona solo si ambos usuarios estan con vida
+
+            System.out.println("----- Ronda " + ronda + "-----");
+
+            turn(user1,user2); // se ejecuta el primer ataque
+
+            if (user2.isAlive()){ // se evalua si el personaje esta vivo si es asi
+                turn(user2,user1); // se ejecuta el segundo ataque invirtiendo los roles
+            }
+            ronda++; // se aumenta la vaeiable ronda en 1
+        }
+
+        // cuando uno de los dos quede en cero osea isAlive es false
+        // se evalua y se da el veredicto con un if condicional
+
+        if (user1.isAlive()){
+            System.out.println(" jugador " + user1.getName() + " a ganado la batalla!!");
+        }else{
+            System.out.println(" jugador " + user2.getName() + " a ganado la batalla!!");
+        }
+    }
+
 
 }
-/**
- * me falta implementar el metodo starFight y crear las herencias de la clase Character
- * y la clase Weapon
- */
+
