8d37c2c5adf870abd19a0512c6b52fbb980f9308
Carlos Andres Castrillon Oviedo <carlos.castrillonov@campusucc.edu.co>
2025-05-20 16:36:32 -0500

    subclase HombreLobo

---
+++ src/HombreLobo.java
@@ -0,0 +1,16 @@
+public class HombreLobo extends Character{
+    public HombreLobo(String name, int lifePoints, Weapon weapon){
+        super(name,"Alpha tribu de los crecientes",lifePoints,weapon);
+    }
+    @Override // permite sobre escribir un metodo
+    public void atackSimple (Character opponent){
+        System.out.println("Aturde con aullido!");
+        super.atackSimple(opponent);
+
+    }
+    @Override
+    public void attackWithWeapon(Character opponent){
+        System.out.println(this.getName() + " usa su Arma!");
+        super.attackWithWeapon(opponent);
+    }
+}
